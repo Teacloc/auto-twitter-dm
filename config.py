3677f74dda3df8de69feae73de52756b5cf9dd8b
Teacloc <32007235+Teacloc@users.noreply.github.com>
2017-12-01 14:13:18 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # configure the dm text
 def generate_dm_text(name):
-	return '''Thanks for the follow {}. Welcome to my online community! Catch my next show on mixer.com/Teacloc'''.format(name)
+	return '''Welcome to my online community, {}! Catch my next show on mixer.com/Teacloc'''.format(name)
 
 scheduler_time = 15 #in minutes
 
